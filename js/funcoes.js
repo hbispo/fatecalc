142f6e8089155146fed73f8492ba87566251c402
Henrique Bispo dos Santos <henrique@criodigital.com.br>
2021-05-17 22:41:10 -0300

    Variáveis mensais e anuais nas fórmulas

--- js/funcoes.js
+++ js/funcoes.js
@@ -8,7 +8,7 @@ window.onload = function () {
                 signed: (inputs[i].getAttribute('data-negativo') == 1),
                 mapToRadix: []
             });
-            inputs[i].addEventListener('keydown', limparFormula);
+            inputs[i].addEventListener('input', limparFormula);
         } else {
             inputs[i].addEventListener('change', limparFormula);
         }
@@ -32,10 +32,35 @@ function calcular(event = false) {
     let formulasTemp = formulas.slice();
     for (let i = 0; i < campos.length; ++i) {
         escopo[campos[i].id] = campos[i].mask.typedValue;
+        let valorMensal = campos[i].mask.typedValue;
+        let valorAnual = campos[i].mask.typedValue;
 
-        let busca = new RegExp(' ' + campos[i].id + ' ', 'g');
-        for (let j = 0; j < formulasTemp.length; ++j) {
-            formulasTemp[j].formulaTex = formulasTemp[j].formulaTex.replace(busca, ' ' + campos[i].mask.value + ' ');
+        let campoPeriodo = document.forms[0].elements[campos[i].id + 'Periodo'];
+        if (campoPeriodo) {
+            if (campoPeriodo.value == 'M') {
+                valorAnual *= 12;
+            } else {
+                valorMensal /= 12;
+            }
+            escopo[campos[i].id + 'mensal'] = valorMensal;
+            escopo[campos[i].id + 'anual'] = valorAnual;
+        }
+
+        if (campos[i].mask.value != '') {
+            let busca = new RegExp(' ' + campos[i].id + ' ', 'g');
+            let buscaMensal;
+            let buscaAnual;
+            if (campoPeriodo) {
+                buscaMensal = new RegExp(' ' + campos[i].id + '\_\{mensal\} ', 'g');
+                buscaAnual = new RegExp(' ' + campos[i].id + '\_\{anual\} ', 'g');
+            }
+            for (let j = 0; j < formulasTemp.length; ++j) {
+                formulasTemp[j].formulaTex = formulasTemp[j].formulaTex.replace(busca, ' ' + campos[i].mask.value + ' ');
+                if (campoPeriodo) {
+                    formulasTemp[j].formulaTex = formulasTemp[j].formulaTex.replace(buscaMensal, ' ' + valorMensal.toString().replace('.', ',') + ' ');
+                    formulasTemp[j].formulaTex = formulasTemp[j].formulaTex.replace(buscaAnual, ' ' + valorAnual.toString().replace('.', ',') + ' ');
+                }
+            }
         }
     }
     for (let i = 0; i < formulasTemp.length; ++i) {
