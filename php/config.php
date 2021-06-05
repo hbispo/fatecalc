26f043a1552036b39d54caef9e4885425180891d
Henrique Bispo <henrique@obispo.com.br>
2021-06-05 19:02:01 -0300

    Volta do campo atualização e alteração do rodapé

--- php/config.php
+++ php/config.php
@@ -5,10 +5,9 @@ final class Config
     //Dados de conexão: endereço, usuário, senha, banco de dados
     const banco = ['localhost', 'fatecalc', 'fatecalc', 'fatecalc'];
 
-    public static function rodape()
-    {
-        return '<footer class="text-center">
+    const atualizacao = 0;
+
+    const rodape = '<footer class="text-center">
             Desenvolvido por <a href="https://github.com/hbispo">Henrique Bispo</a> sob uma <a href="https://github.com/hbispo/fatecalc/blob/master/LICENSE">licença MIT</a>. Código-fonte no <a href="https://github.com/hbispo/fatecalc">GitHub</a>.
         </footer>';
-    }
 }
