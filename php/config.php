279957a13f29f97d6c3d8d3eb88e5b2aea9491ac
Henrique Bispo <henrique@obispo.com.br>
2021-06-05 18:34:06 -0300

    Remoção de configurações não utilizadas

--- php/config.php
+++ php/config.php
@@ -5,18 +5,6 @@ final class Config
     //Dados de conexão: endereço, usuário, senha, banco de dados
     const banco = ['localhost', 'fatecalc', 'fatecalc', 'fatecalc'];
 
-    const atualizacao = 0;
-
-    /*Para acrescentar um item no menu, seguir o formato
-    * ['Texto', 'icone-do-font-awesome', 'pagina.php', [submenu]]
-    * (ícones, páginas e submenus opcionais)
-    */
-    const menu = [
-        ['Home', 'home', './', 1],
-        ['Tutorial', 'question-circle', 'tutorial.php', 1],
-        ['Sair', 'sign-out-alt', 'login.php', 1]
-    ];
-
     public static function rodape()
     {
         return '<footer class="text-center">
